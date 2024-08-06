b4dcead53d496cb9f3d400a47c40505a82497552
Atharv <atharvniprul74@gmail.com>
2024-08-06 16:32:42 +0530

    Site Completed by HTML & CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+let water=document.getElementById("waterfall")
+let hilr=document.getElementById("Hills")
+let ocea=document.getElementById("Ocean")
+let rn=document.getElementById("Rain")
+let mount=document.getElementById("Mountain")
+let tres=document.getElementById("Trees")
+let bech=document.getElementById("Beach")
+
+
